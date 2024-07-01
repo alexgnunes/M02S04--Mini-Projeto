0c6abd9fee511fb15380cf29657758a284a15711
Alex Nunes <alexgaribalde_tb@hotmail.com>
2024-07-01 19:47:31 -0300

    criação da classe base Conta

---
+++ src/Conta.java
@@ -0,0 +1,27 @@
+public abstract class Conta {
+    private String titular;
+    private double saldo;
+
+    public Conta(String titular, double saldo) {
+        this.titular = titular;
+        this.saldo = saldo;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void depositar(double valor) {
+        if (valor > 0) {
+            saldo += valor;
+        }
+    }
+
+    public abstract void sacar(double valor);
+
+    public abstract void exibirDetalhes();
+}
