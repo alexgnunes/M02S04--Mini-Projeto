8456d894578397a40e562e515f7d1938bd17f3ec
Alex Nunes <alexgaribalde_tb@hotmail.com>
2024-07-09 05:29:53 -0300

    Criando a classe para a conta corrente

--- src/Conta.java
+++ src/Conta.java
@@ -15,6 +15,10 @@ public abstract class Conta {
         return saldo;
     }
 
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
     public void depositar(double valor) {
         if (valor > 0) {
             saldo += valor;
---
+++ src/ContaCorrente.java
@@ -0,0 +1,28 @@
+public class ContaCorrente extends Conta {
+    private double limite;
+
+    public ContaCorrente(String titular, double saldo, double limite) {
+        super(titular, saldo);
+        this.limite = limite;
+    }
+
+    public double getLimite() {
+        return limite;
+    }
+
+    @Override
+    public void sacar(double valor) {
+        if (valor > getSaldo() + this.limite) {
+            throw new IllegalArgumentException("Saldo insuficiente para realizar o saque.");
+        }
+        setSaldo(getSaldo() - valor);
+    }
+
+    @Override
+    public void exibirDetalhes() {
+        System.out.println("Titular: " + getTitular());
+        System.out.println("Saldo: " + getSaldo());
+        System.out.println("Limite: " + limite);
+    }
+
+}
